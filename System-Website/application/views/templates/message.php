3e00a6d011a5fa75007ac6f88b4ee1afee5f6794
Toni <K>
2019-02-15 14:26:46 +0200

    Revert "Project files"
    This reverts commit 2807e05931f121f588c61695810b9d57b1886d09.

--- System-Website/application/views/templates/message.php
+++
@@ -1,8 +0,0 @@
-<br>
-<div class="row justify-content-center">
-  <div class="col-auto">
-      <div class="alert alert-danger text-center" role="alert">
-        <?php echo $message; ?>
-      </div>
-    </div>
-</div>
